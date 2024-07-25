299c78d991e4e22b34cc83f0b671d21fe61b614e
Daniel Atonal <danielatonal8@gmail.com>
2024-07-24 19:18:35 -0600

    Contact Page added

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -2,28 +2,33 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomePageComponent } from "./app/shared/pages/home-page/home-page.component";
 import { AboutPageComponent } from "./app/shared/pages/about-page/about-page.component";
+import { ContactComponent } from "./app/shared/pages/contact/contact.component";
 
-const routes: Routes= [
-    {
-        path:"home",
-        component: HomePageComponent
-    },
-    {
-        path:"about",
-        component:AboutPageComponent
-    },
-    {
-        path:"**",
-        redirectTo: "home"
-    }
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomePageComponent
+  },
+  {
+    path: "about",
+    component: AboutPageComponent
+  },
+  {
+    path: "contact",
+    component: ContactComponent
+  },
+  {
+    path: "**",
+    redirectTo: "home"
+  }
 ];
 
 @NgModule({
-    imports:[
-        RouterModule.forRoot(routes),
-    ],
-    exports:[
-        RouterModule,
-    ]
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule,
+  ]
 })
-export class AppRoutingModule{}
+export class AppRoutingModule { }
